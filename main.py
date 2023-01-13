3b3497086e944f450e5b4a7e8653c368f2b3ef00
henrry bourgeot <henrrybrgt@gmail.com>
2023-01-12 22:43:18 -0400

    añadido funcionamiento de casi todas las funciones de agregar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from database import SesionLocal, engine, Base
 import model
 
 # FastAPI
-from fastapi import FastAPI, Depends, Request, Body
+from fastapi import FastAPI, Depends, Body
 from fastapi.exceptions import HTTPException
 # SQLAlchemy
 from sqlalchemy.orm import Session
@@ -19,39 +19,48 @@ Base.metadata.create_all(bind=engine)
   GET = Obtener
   POST = Crear
   PATCH = Actualizar parcialmente
+  PUT = actualizar algo completamente
   DELETE = Borrar
 
   ¿Y qué son estos? son los métodos de HTTP, recomiendo que lean más acerca de ellos
 """
 
 # TODO: continuar el crud de empleado
-# TODO DONE 11/1/2023: login, crear_empleado, crear_promotors
+# TODO DONE 11/1/2023: login, crear_empleado, crear_promotor
+# TODO DONE 12:1/2023: terminadas rutas POST :D
 
 # Abrimos la conexion con la base de datos
 
-db: Session
+connection: Session
 
 
 def obtener_bd():
-  global db
+  global connection
   try:
-    db = SesionLocal()
-    return db
+    connection = SesionLocal()
+    return connection
   finally:
-    db.close()
+    connection.close()
 
 # Operaciones de proyectos
 
 
 @app.get("/proyectos")
-def mostrar_proyectos(request: Request, db: Session = Depends(obtener_bd)):
+def mostrar_proyectos(db: Session = Depends(obtener_bd)):
   return {"proyectos": "Not finished"}
 
 
 @app.post("/crear/proyecto")
-def nuevo_proyecto(proyecto: model.Proyectos = Body(...), db: Session = Depends(obtener_bd)):
-  # crea un proyecto
-  return {"proyectos": "Not finished"}
+def agregar_proyecto(proyecto: model.Proyectos = Body(...), db: Session = Depends(obtener_bd)):
+  try:
+    nuevo_proyecto = schemas.Proyectos(**proyecto.dict())  # creamos al nuevo empleado
+    db.add(nuevo_proyecto)  # lo agregamos a la bd
+    db.commit()  # confirmamos la inserción
+    db.refresh(nuevo_proyecto)  # refrescamos los valores en la variable
+
+    return {"estado": "exitoso", "empleado": nuevo_proyecto}  # retornamos al nuevo empleado
+  except Exception as e:
+    raise HTTPException(400, str(e))
 
 
 @app.patch("/modificar/proyecto")
@@ -81,9 +90,16 @@ def mostrar_tareas(db: Session = Depends(obtener_bd)):
 
 
 @app.post("/crear/tarea")
-def nueva_tarea(tarea: model.Tareas = Body(...), db: Session = Depends(obtener_bd)):
-  # crea una tarea
-  return
+def agregar_tarea(tarea: model.Tareas = Body(...), db: Session = Depends(obtener_bd)):
+  try:
+    nueva_tarea = schemas.Tareas(**tarea.dict())  # creamos al nuevo empleado
+    db.add(nueva_tarea)  # lo agregamos a la bd
+    db.commit()  # confirmamos la inserción
+    db.refresh(nueva_tarea)  # refrescamos los valores en la variable
+
+    return {"estado": "exitoso", "tarea": nueva_tarea}  # retornamos al nuevo empleado
+  except Exception as e:
+    raise HTTPException(400, str(e))
 
 
 @app.patch("/modificar/tarea")
@@ -112,13 +128,27 @@ def docs_tarea(db: Session = Depends(obtener_bd)):
   return
 
 
-@app.post("/tarea/{tareaId}/crear/documento")
-def crear_doc(db: Session = Depends(obtener_bd)):
-  # crea un documento
+@app.post("/tarea/{tarea_id}/crear/documento")
+def crear_doc(tarea_id: int, documento: model.Documentos, db: Session = Depends(obtener_bd)):
+  try:
+    documento.codigo_tarea = tarea_id
+    nuevo_doc = schemas.Empleado(**documento.dict())  # creamos al nuevo empleado
+    db.add(nuevo_doc)  # lo agregamos a la bd
+    db.commit()  # confirmamos la inserción
+    db.refresh(nuevo_doc)  # refrescamos los valores en la variable
+
+    return {"estado": "exitoso", "documento": nuevo_doc}  # retornamos al nuevo empleado
+  except Exception as e:
+    raise HTTPException(400, str(e))
+
+
+@app.put("/tarea/{tarea_id}/modificar/{documento_id}")
+def modificar_documento(db: Session = Depends(obtener_bd)):
+  # modifica un documento
   return
 
-
 # Operaciones de usuario
+
 
 @app.get("/empleados")
 def obtener_empleados(db: Session = Depends(obtener_bd)):
@@ -146,7 +176,7 @@ def crear_empleado(empleado: model.Empleado = Body(...), db: Session = Depends(o
 
 
 @app.post("/nuevo/promotor")
-def crear_promotor(request: Request, promotor: model.Promotor = Body(...), db: Session = Depends(obtener_bd)):
+def crear_promotor(promotor: model.Promotor = Body(...), db: Session = Depends(obtener_bd)):
   try:
     nuevo_promotor = schemas.Promotor(**promotor.dict())  # creamos al nuevo promotor
     db.add(nuevo_promotor)                                # mismo que función anterior
@@ -159,7 +189,7 @@ def crear_promotor(request: Request, promotor: model.Promotor = Body(...), db: S
 
 
 @app.post("/login")
-def login_post(request: Request, usuario: model.PromotorLogin, db: Session = Depends(obtener_bd)):
+def login_post(usuario: model.PromotorLogin, db: Session = Depends(obtener_bd)):
   try:  # este bloque de código ejecuta to do lo que se encuentre dentro
     promotores = db.query(schemas.Promotor).all()  # obtenemos los promotores
     for promotor in promotores:  # iteramos los promotores
@@ -167,18 +197,12 @@ def login_post(request: Request, usuario: model.PromotorLogin, db: Session = Dep
       # lo que está dentro de filter, es literalmente esta sentencia: WHERE usuario = "usuario" AND contra = "contra"
       usuario_login = db.query(schemas.Promotor).filter(usuario.usuario == promotor.usuario and
                                                         usuario.contra == promotor.contra).first()
-    if usuario_login is None: # si no conseguimos al usuario
+    if usuario_login is None:  # si no conseguimos al usuario
       return {"confirmado": "no"}  # no estara confirmado
     else:  # de lo contrario
       return {"confirmado": "si"}  # estará confirmado
   except Exception as e:  # si al ejecutar algo sucede un error o excepcion, se ejecutara esto
     raise HTTPException(400, str(e))
-
-
-@app.get("/empleado/{empleadoId}/detalles")
-def empleado_detalles(db: Session = Depends(obtener_bd)):
-  # mostrar detalles del empleado
-  return
 
 
 @app.patch("/modificar/empleado")
